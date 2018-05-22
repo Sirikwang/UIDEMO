db3cdb6bb0521d3f598da9d8a2f1ae7340e269f1
Siriwan <Narak_gvang@hotmail.com>
2018-05-22 16:03:13 +0700

    input login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,47 @@
 package sample;
 
-public class Controller {
-}
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+ private final String id = "admin";
+ private final String pass = "1234";
+
+    @FXML
+    private TextField Username;
+    @FXML
+    private PasswordField Password;
+    @FXML
+    private Button btnLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void Login(ActionEvent event){
+        try {
+            if (id.equals(Username.getText()) && pass.equals(Password.getText())) {
+                JOptionPane.showMessageDialog(null,"Welcome to MIT SAIYAI APP");
+            }
+            else {
+                JOptionPane.showMessageDialog(null,"Your Username or Password is invalid.","Warning Message",JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }//Login
+
+}//class
---
+++ src/sample/loginModel.java
@@ -0,0 +1,27 @@
+package sample;
+
+import dbUtil.dbConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class loginModel {
+
+    Connection connection;
+
+
+    //constuctor
+    public loginModel(){
+        try {
+            this.connection = dbConnection.getConnection()
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (this.connection == null) System.exit(1);
+    }
+
+    public boolean isDatabaseConnection(){
+        return this.connection != null;
+    }
+
+}//class
