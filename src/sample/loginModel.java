3449ce97377f949811accd1bda52475731653b39
Siriwan <Narak_gvang@hotmail.com>
2018-05-22 17:00:59 +0700

    input login up

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,6 +3,8 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
@@ -13,7 +15,7 @@ public class loginModel {
     //constuctor
     public loginModel(){
         try {
-            this.connection = dbConnection.getConnection()
+            this.connection = dbConnection.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -23,5 +25,31 @@ public class loginModel {
     public boolean isDatabaseConnection(){
         return this.connection != null;
     }
+
+    public boolean isLogin(String Username, String Password) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from admin where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,Username);
+            pr.setString(2,Password);
+
+            rs = pr.executeQuery();
+            if (rs.next()){
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        finally {
+            pr.close();
+            rs.close();
+        }
+    }
+
 
 }//class
